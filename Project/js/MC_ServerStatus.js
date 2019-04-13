053e36ab5fad284eaeae7bc299df7ac0e7f4f560
Miheel <mikaelleuf@gmail.com>
2019-04-13 16:48:08 +0200

    info outline

--- Project/js/MC_ServerStatus.js
+++ Project/js/MC_ServerStatus.js
@@ -1,4 +1,32 @@
-function comp(UPin) {
+/*--------The code snippet below is provided by https://mcapi.us/--------*/
+/*-----------------------------------------------------------------------*/	
+
+	//MinecraftAPI.getServerStatus('StarMC.ddns.net', {
+	MinecraftAPI.getServerStatus('play.minesuperior.com', {
+		port: 25565 // optional, only if you need a custom port
+	}, function(err, status) {
+		if (err) {
+			return document.querySelector('.server-status').innerHTML = 'Error loading status';
+		}
+		// you can change these to your own message!
+		document.querySelector('.server-online').innerHTML = status.online ? 'up' : 'down';
+		
+		var status_ = 0;
+		
+		if (status.online === true) {
+			status_ = "up";
+		} else if (status.online === false) {
+			status_ = "down";
+		} else {
+			status_ = "err";
+		}
+		
+		compstr(status_);
+	});
+/*-----------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------*/	
+
+function compstr(UPin) {
 	
 	if ("up" === UPin) {
 		//server is up
